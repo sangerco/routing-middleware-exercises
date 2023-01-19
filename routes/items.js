5784ad305c602b7403eb1002fcf7dd1d5a27515e
Dan Sanger <sangerco@yahoo.com>
2023-01-19 11:46:59 -0700

    updated test and requirements

--- routes/items.js
+++ routes/items.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const router = new express.Router();
-const ExpressError = require("../expressError");
-const items = require('../fakeDb');
+const express = require("express")
+const router = new express.Router()
+const ExpressError = require("../expressError")
+const items = require('../fakeDb')
 
 router.get('/items', function(req,res) {
-    return res.json({ items });
+    res.json({ items })
 })
 
 router.post('/items', function (req, res, next) {
     try {
         if(!req.body.name) throw new ExpressError('Item must have a name!', 400);
         if(!req.body.price) throw new ExpressError('Item must have a price!', 400);
-        const newItem = { name: req.body.name, price: req.body.price };
-        items.push(newItem);
-        return res.status(201).json({ added: {newItem} });
+        const newItem = { name: req.body.name, price: req.body.price }
+        items.push(newItem)
+        return res.status(201).json({ added: {newItem} })
     } catch (e) {
         return next(e)
     }
@@ -24,7 +24,7 @@ router.get('/items/:name', function (req, res) {
     if(foundItem === undefined) {
         throw new ExpressError('Item not found!', 404);        
     } 
-    return res.json({ item: foundItem });
+    res.json({ item: foundItem });
 })
 
 router.patch('/items/:name', function (req, res) {
@@ -34,7 +34,7 @@ router.patch('/items/:name', function (req, res) {
     }
     const updatedItem = { name: req.body.name, price: req.body.price };
     foundItem = updatedItem;
-    return res.status(200).json({ updated: {updatedItem} });
+    res.json({ updated: {updatedItem} });
 })
 
 router.delete('/items/:name', function (req, res) {
@@ -43,7 +43,7 @@ router.delete('/items/:name', function (req, res) {
         throw new ExpressError('Item not found!', 404); 
     }
     items.splice(foundItem, 1);
-    res.status(200).json({ message: 'Deleted' });
+    res.json({ message: 'Deleted' });
 })
 
 module.exports = router;
--- routes/items.test.js
+++ routes/items.test.js
@@ -21,7 +21,7 @@ describe('GET root', () => {
     test('Root should be empty', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(404);
-        expect(res.body).toEqual('Not Found');
+        expect(res.body).toEqual('Not Found')
     })
 })
 
@@ -29,10 +29,64 @@ describe('GET /items', () => {
     test('show items page', async () => {
         const res = await request(app).get('/items');
         expect(res.statusCode).toBe(200);
-        expect(res.body).toEqual([{peanutButter}, {jelly}]);
+        expect(res.body).toEqual([{peanutButter}, {jelly}])
     })
 })
 
-// describe('POST /items', () => {
-//     test
-// })
+describe('POST /items', () => {
+    test('post new item', async () => {
+        const res = await request(app).post('/items').send({name: 'oatmeal', price: 4.99});
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toEqual({ "added": {"name": "oatmeal", "price": 4.99}});
+    })
+    test('respond with 400 for empty name', async () => {
+        const res = await request(app).post('/items').send({ price: 5.00 });
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toEqual('Item must have a name!');
+    })
+    test('respond with 400 for empty price', async () => {
+        const res = await request(app).post('/items').send({ name: 'tacos'});
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toEqual('Item must have a price!');
+    })
+})
+
+
+describe('GET /items/:name', () => {
+    test('show individual item page', async () => {
+        const res = await request(app).get(`/items/${jelly.name}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual(`${jelly}`);
+    })
+    test('show 404 response for invalid name', async () => {
+        const res = await request(app).get('/items/beefjerky');
+        expect(res.statusCode).toBe(404);
+        expect(res.body).toEqual('Item not found!');
+    })
+})
+
+describe('PATCH /items/:name', () => {
+    test('update item data', async () => {
+        const res = await request(app).patch(`/items/${peanutButter.name}`).send({ name: "nutella", price: 4.00 });
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ "updated": {"name": "nutella", "price": 4.00} })
+    })
+    test('respond with 404 for invalid item', async () => {
+        const res = await (await request(app).patch('/items/noodles')).send({name: "doesn't matter", price: 'no gonna send'});
+        expect(res.statusCode).toBe(404);
+        expect(res.body).toEqual('Item not found!');
+    })
+})
+
+describe('DELETE /items/:name', () => {
+    test('update item data', async () => {
+        const res = await request(app).delete(`/items/${jelly.name}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ message: "Deleted" })
+    })
+    test('respond with 404 for invalid name', async () => {
+        const res = await request(app).delete('/items/apples');
+        expect(res.statusCode).toBe(404);
+        expect(res.body).toEqual('Item not found!');
+    })
+})
